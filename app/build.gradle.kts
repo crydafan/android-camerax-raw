599df346893d1ccf1206545b1ae188ba99830421
Leonardo <leonardo.estacio.h@gmail.com>
2026-02-23 19:17:26 -0500

    feat: simple CameraX example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    kotlin("plugin.serialization") version "2.0.21"
+    alias(libs.plugins.androidx.navigation.safeargs)
 }
 
 android {
@@ -29,6 +31,10 @@ android {
             )
         }
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -39,7 +45,24 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("androidx.concurrent:concurrent-futures:1.3.0")
+    // Kotlin
+    implementation("androidx.concurrent:concurrent-futures-ktx:1.3.0")
+    implementation("androidx.navigation:navigation-compose:2.9.7")
+    implementation(libs.androidx.navigation.fragment)
+    implementation("androidx.navigation:navigation-ui:2.9.7")
+    // Feature module support for Fragments
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.9.7")
+    implementation("androidx.camera:camera-core:1.5.3")
+    implementation("androidx.camera:camera-camera2:1.5.3")
+    implementation("androidx.camera:camera-lifecycle:1.5.3")
+    implementation("androidx.camera:camera-view:1.1.0-beta01")
+    implementation("androidx.camera:camera-extensions:1.5.3")
+    implementation("androidx.window:window:1.5.1")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
